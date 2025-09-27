6f4c2017d999e08f752bd3d3f94dd065dee91861
Joel  Rojas <135097285+jr95jr@users.noreply.github.com>
2025-09-26 20:30:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import io, os, sqlite3
-    import streamlit as st
+import streamlit as st
 
     st.set_page_config(page_title="Demo: Pruebas Unitarias e Integración en Python", layout="wide")
     st.title("🧪 Demo Didáctica: Pruebas Unitarias e Integración en Python")
